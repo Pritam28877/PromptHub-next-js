48a1af9a1839a7ef638e4da062e1eddb5c9d94ce
Pritam Das <dpritam28577@gmail.com>
2023-09-02 02:58:10 +0530

    add the image funtion

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,20 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+        />
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
