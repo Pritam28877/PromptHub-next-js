b20b19c1794f11a633d8e2d5469d3c647b5ff61f
Pritam Das <dpritam28577@gmail.com>
2023-09-02 03:23:03 +0530

    add the create post route btn

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,10 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isUserLoggedIN, setIsUserLoggedIN] = useState(true);
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -11,8 +13,23 @@ const NavBar = () => {
           alt="logo"
           width={30}
           height={30}
+          className="object-contain"
         />
+        <p className="logo_text">PromptHub</p>
       </Link>
+      {/* Mobile Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIN ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              {" "}
+              Create Post
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
