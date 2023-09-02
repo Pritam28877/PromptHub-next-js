ae46d74b2620c4640638c565faf04aab0a90d92a
Pritam Das <dpritam28577@gmail.com>
2023-09-02 16:22:50 +0530

    add the provider auth for the mobile version

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -67,6 +67,35 @@ const NavBar = () => {
           </>
         )}
       </div>
+      {/* Mobile navigation  */}
+      <div className="sm:hidden flex relative">
+        {isUserLoggedIN ? (
+          <div className="flex">
+            <Image
+              src="assets/images/logo.svg"
+              width={37}
+              height={37}
+              className="rounded-full"
+              alt="profile"
+              onClick={() => {}}
+            />
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className="black_btn"
+                >
+                  signIn
+                </button>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
