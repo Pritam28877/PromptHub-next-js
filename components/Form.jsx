9ee9113960db80390b8d51b72ee94f2179c30bfe
Pritam Das <dpritam28577@gmail.com>
2023-09-03 14:25:49 +0530

    add the text area to the form

--- components/Form.jsx
+++ components/Form.jsx
@@ -7,6 +7,25 @@ const Form = ({ type, post, setPost, handleSubmit }) => {
       <h1 className="head_text text-left">
         <span className="blue_gradient">{type} Post</span>
       </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the world, and let your
+        imagination run wild with any AI-powered platform
+      </p>
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+            className="form_textarea"
+          />
+        </label>
+      </form>
     </section>
   );
 };
