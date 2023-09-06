1bea3e37878d51b791bc00b99efe9344a2553b6d
Pritam Das <dpritam28577@gmail.com>
2023-09-06 14:52:15 +0530

    fix the data passing issue for the session

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,25 +1,28 @@
 "use client";
-import React from "react";
-import Profile from "@components/Profile";
 
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-const ProfilePage = () => {
+
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
   const router = useRouter();
-  const [myPosts, setMyPosts] = useState([]);
   const { data: session } = useSession();
+
+  const [myPosts, setMyPosts] = useState([]);
+
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
-
       setMyPosts(data);
     };
 
     if (session?.user.id) fetchPosts();
   }, [session?.user.id]);
 
+  console.log(myPosts);
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
@@ -43,17 +46,16 @@ const ProfilePage = () => {
       }
     }
   };
+
   return (
-    <>
-      <Profile
-        name="My"
-        desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
-        data={myPosts}
-        handleEdit={handleEdit}
-        handleDelete={handleDelete}
-      />
-    </>
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
   );
 };
 
-export default ProfilePage;
+export default MyProfile;
